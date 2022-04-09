2eb6f1bbf0985a91093cc34385ab9c7cde5b172c
logan818 <logan.calhoun45@gmail.com>
2022-04-09 02:23:17 -0400

    its working yeet

---
+++ facial-recognition-app/src/README.md
@@ -0,0 +1,3 @@
+CREDIT: 
+Nicholas Renotte
+Youtube Video Link: https://www.youtube.com/watch?v=7lXYGDVHUNw&t=633s
--- facial-recognition-app/src/utilities.js
+++ facial-recognition-app/src/utilities.js
@@ -2643,11 +2643,41 @@ export const TRIANGULATION = [
   ];
 
 // Draw triangle
+const drawPath = (ctx, points, closePath) => {
+    const region = new Path2D();
+    region.moveTo(points[0][0], points[0][1]);
+    for (let i = 1; i < points.length; i++){
+        const point = points[i];
+        region.lineTo(point[0], point[1]);
+    }
+    if (closePath) {
+        region.closePath();
+    }
+    ctx.strokeStyle = "aqua";
+    ctx.stroke(region);
+}
+
+
+
 // Draw the points
 export const drawMesh = (prediction, ctx) => {
     if(prediction.length>0){
+
+        //Draw the points
         prediction.forEach(prediction=>{
             const keypoints = prediction.scaledMesh;
+
+        //Draw Triangles
+        for (let i = 0; i < TRIANGULATION.length / 3; i++){
+            const points = [
+                TRIANGULATION[i * 3],
+                TRIANGULATION[i * 3 + 1],
+                TRIANGULATION[i * 3 + 2],
+            ].map((index) => keypoints[index]);
+            drawPath(ctx, points, true);
+        }
+
+
             for(let i = 0; i < keypoints.length; i++){
                 const x = keypoints[i][0];
                 const y = keypoints[i][1];
